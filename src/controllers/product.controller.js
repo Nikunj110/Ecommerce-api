c2fc2815e72cb84e62063107768c16ab2b8d2eac
Nikunj <nikunjgajera2550@gmail.com>
2025-11-14 11:50:19 +0530

    feat: add get, update, and delete product functionalities to product controller and routes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -23,10 +23,10 @@ const createProduct = async (req, res) => {
     res.status(201).json(newProduct);
 
   } catch (error) {
-    if (error.name==='ValidationError') {
+    if (error.name === 'ValidationError') {
       return res.status(400).json({
-        message:'Validation Error ',
-        error:error.message
+        message: 'Validation Error ',
+        error: error.message
       });
     }
 
@@ -38,4 +38,90 @@ const createProduct = async (req, res) => {
   }
 }
 
-export { getAllProducts, createProduct }
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error in Finding Products',
+      error: error.message
+
+    });
+  }
+}
+
+const updateProduct = async (req, res) => {
+  try {
+    const updateData = req.body;
+    const { id } = req.params;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      updateData,
+      {
+        new: true, // This option returns the *updated* document
+        runValidators: true // This runs your schema validators (like min: 0 on price)
+      }
+    )
+
+    if (!updatedProduct) {
+      res.status(404).json({
+        message: 'Product Not Found'
+      })
+    }
+
+    res.status(200).json(updatedProduct)
+
+
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(400).json({
+        message: 'Validation Error',
+        error: error.message
+      })
+    }
+
+    res.status(500).json({
+      message: 'Error While Updating Product ',
+      error: error.message
+    })
+  }
+}
+
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(
+      id,
+      // {
+      //   new: true,
+      // }
+    )
+    if (!deletedProduct) {
+      res.status(404).json({
+        message: 'Product NOt Found ',
+      })
+    }
+
+    res.status(200).json({
+      message: 'Product deleted successfully',
+      id: deletedProduct._id
+
+    })
+  } catch (error) {
+
+    res.status(500).json({
+      message: 'Error Occured While Delete Product',
+      error: error.message
+    })
+  }
+}
+
+export { getAllProducts, createProduct, getProduct, updateProduct, deleteProduct }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,21 @@
 import express from 'express'
-import { getAllProducts,createProduct } from '../controllers/product.controller.js';
+
+import {
+  getAllProducts,
+  createProduct,
+  getProduct
+  , updateProduct,
+  deleteProduct
+  
+} from '../controllers/product.controller.js';
 
 const router = express.Router();
 
-router.get('/',getAllProducts)
+router.get('/', getAllProducts);
 
-router.post('/',createProduct)
+router.post('/', createProduct);
+router.get('/:id', getProduct);
+router.patch('/:id', updateProduct);
+router.delete('/:id',deleteProduct)
 
 export default router;
