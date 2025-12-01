2131541bbb701e3988399f46b3d1f367d4cd4f6c
Nikunj <nikunjgajera2550@gmail.com>
2025-12-01 19:15:44 +0530

    Modified Admin Middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,34 +1,22 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import 'dotenv/config';
-
-const authorizeAdmin = async (req, res, next) => {
+const authorizeAdmin = (req,res,next) =>{
   try {
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({
-        message: 'Admin Acess Denied',
-        error: error.message
+    if (!req.user) {
+      return res.status(401).json({
+        message:'Access Denied. User not authenticated.',
       })
     }
-    const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(401).json({
-        message: 'Access Denied. No token provided.'
-      });
+    if (req.user.role!=='admin') {
+      return res.status(403).json({
+        message:'Access Denied Admin Only'
+      })
+
+      next();
     }
-
-    const token = authHeader.split(' ')[1];
-    
-    const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
-
-    req.user = decoded;
-    next();
-
   } catch (error) {
-    return res.status(400).json({
-      message: "Error Occurs When You Fetch The Admin Data",
-      error: error.message
+    res.status(500).json({
+      message:'Error In Admin Middleware',
+      error:error.message
     })
   }
 }
