48089bcfce5b6b6ad48e5795f8740b78b4aedae4
Nikunj <nikunjgajera2550@gmail.com>
2025-12-12 22:39:58 +0530

    Add route for fetching user orders

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addOrderItems } from "../controllers/order.controller.js";
+import { addOrderItems ,getMyOrders} from "../controllers/order.controller.js";
 import {authenticateUser} from "../middlewares/auth.middleware.js"
 
 
 const router = express.Router();
 router.post('/', authenticateUser, addOrderItems);
+router.post('/myorders',authenticateUser,getMyOrders);
 
 export default router;
